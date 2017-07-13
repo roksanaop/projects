7a1ba2dc88a8196540c1e33e8ff40a06e7c604ed
roksanaop <roksanaop@tlen.pl>
2017-07-13 15:30:05 +0200

    Update video-searcher.component.js

--- app/components/video-searcher/video-searcher.component.js
+++ app/components/video-searcher/video-searcher.component.js
@@ -7,8 +7,6 @@
       templateUrl: 'app/components/video-searcher/video-searcher.template.html',
       controller: 'VideoSearcherCtrl',
       controllerAs: 'app'
-//const APIkey = "AIzaSyAG_j5vdipqwk28k2ozPLDqvb4cDAdMJHY";
-//<iframe id="player" type="text/html" width="640" height="360" src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com" frameborder="0"></iframe>
     });
 
 })();
