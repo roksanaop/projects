58045bb41c00871b498da6aa53246543a69185df
roksanaop <roksanaop@tlen.pl>
2017-07-28 14:19:39 +0200

    Update YouTubeService.service.js

--- app/services/YouTubeService.service.js
+++ app/services/YouTubeService.service.js
@@ -1,39 +1,38 @@
 (function() {
   'use strict';
 
+  class YouTubeService {
+    constructor($http) {
+      Object.assign(this, {$http});
+    }
+
+    search(id, callback) {
+      var key = 'AIzaSyAG_j5vdipqwk28k2ozPLDqvb4cDAdMJHY';
+      var url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics,snippet&id=' + id + '&key=' + key;
+      return this.$http.get(url)
+        .then(value => {callback(this.prepareData(value));
+          //console.log('Add youtube');
+        });
+    }
+
+    prepareData(data) {
+      let video = data.data.items[0];
+      return this.videoData = {
+        id: video.id,
+        title: video.snippet.title,
+        viewCount: video.statistics.viewCount,
+        likeCount: video.statistics.likeCount,
+        thumbnail: video.snippet.thumbnails.high.url,
+        date: new Date(),
+        favourite: false,
+        embed: '<iframe id="player" type="text/html" width="640" height="360" src="http://www.youtube.com/embed/' + video.id + '" frameborder="0" allowfullscreen></iframe>'
+      }
+    }
+    
+  }
+
   angular
     .module('videoApp')
-    .service('YouTubeService', class YouTubeService {
-      constructor($http, VideoService) {
-        this.$http = $http;
-        this.VideoService = VideoService;
-      }
-
-      search(id) {
-        var key = 'AIzaSyAG_j5vdipqwk28k2ozPLDqvb4cDAdMJHY';
-        var url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics,snippet&id=' + id + '&key=' + key;
-        return this.$http.get(url)
-          .then((response) => {
-            this.VideoService.addVideo(this.prepareData(response));
-            //console.log('Add youtube');
-          });
-      }
-
-      prepareData(data) {
-        let video = data.data.items[0];
-        return this.videoData = {
-          id: video.id,
-          title: video.snippet.title,
-          viewCount: video.statistics.viewCount,
-          likeCount: video.statistics.likeCount,
-          thumbnail: video.snippet.thumbnails.high.url,
-          date: new Date(),
-          favourite: false,
-          embed: '<iframe id="player" type="text/html" width="640" height="360" src="http://www.youtube.com/embed/' + video.id + '" frameborder="0" allowfullscreen></iframe>'
-        }
-        
-      }
-      
-    });
+    .service('YouTubeService', YouTubeService);
 
 })();
