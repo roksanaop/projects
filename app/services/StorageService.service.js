4322a4adb72ad06d7a2b21ec9093cb7e966a2616
roksanaop <roksanaop@tlen.pl>
2017-07-28 14:22:48 +0200

    Update StorageService.service.js

--- app/services/StorageService.service.js
+++ app/services/StorageService.service.js
@@ -1,20 +1,31 @@
 (function() {
   'use strict';
 
+  class StorageService {
+
+    get(keyValue) {
+      return new Promise(resolve => {
+        let videoArr = localStorage.getItem(keyValue);
+        resolve(JSON.parse(videoArr));
+      });
+    }
+
+    set(keyName, keyValue) {
+      return new Promise(resolve => {
+        resolve(localStorage.setItem(keyName, JSON.stringify(keyValue)));
+      });
+    }
+
+    remove(keyName) {
+      return new Promise(resolve => {
+        resolve(localStorage.removeItem(keyName));
+      });
+    }
+
+  }
+
   angular
     .module('videoApp')
-    .service('StorageService', class StorageService {
-      
-      get(keyValue) {
-        var videoArr = localStorage.getItem(keyValue);
-        //console.log(videoArr);
-        return JSON.parse(videoArr);
-      }
-
-      set(keyName, keyValue) {
-        return localStorage.setItem(keyName, JSON.stringify(keyValue));
-      }
-
-    });
+    .service('StorageService', StorageService);
 
 })();
