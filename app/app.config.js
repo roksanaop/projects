2ecf8a99e4a797cfe68947bbb773c17a8ac8b2a8
roksanaop <roksanaop@tlen.pl>
2017-07-28 14:06:55 +0200

    Update app.config.js

--- app/app.config.js
+++ app/app.config.js
@@ -4,18 +4,16 @@
 
   angular
     .module('videoApp')
-    .config(['$locationProvider', '$routeProvider', 
-      function config($locationProvider, $routeProvider) {
-        $locationProvider.hashPrefix('!');
+    .config(['$stateProvider', '$urlRouterProvider',
+      function config($stateProvider, $urlRouterProvider) {
 
-        $routeProvider
-          .when('videos', {
-            template: '<video-searcher></video-searcher>'
-          })
-          .when('/videos/modal', {
-            templateUrl: 'app/controllers/modal-window/modal-window.template.html'
-          })
-          .otherwise('videos');
+        $stateProvider
+          .state('videos', {
+            url: '/videos',
+            template: '<video-searcher></video-searcher><video-list></video-list>'
+          });
+        $urlRouterProvider.otherwise('/videos');
+
       }
     ]);
 
