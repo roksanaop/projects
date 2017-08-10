5fc2cd6e3fd1073941b604d7d647a3a0cf7e4959
roksanaop <roksanaop@tlen.pl>
2017-08-10 12:44:07 +0200

    Update video-list.controller.js

--- app/controllers/video-list.controller.js
+++ app/controllers/video-list.controller.js
@@ -4,30 +4,52 @@
   class VideoListCtrl {
     constructor(StorageService, VideoService) {
       Object.assign(this, {StorageService, VideoService});
-      this.videos = this.VideoService.videos;
-      this.temporaryVideos = this.VideoService.temporaryVideos;
-      this.options = this.VideoService.options;
+      this.videos = this.VideoService.get('videos');
+      this.temporaryVideos = this.VideoService.get('temporaryVideos');
+      this.options = {
+        pageSizes: [5, 10, 20, 50, 100],
+        currentPageSize: 5,
+        currentPage: 0,
+        display: 'list',
+        orderProp: 'title',
+        onlyFavourites: false
+      }
     }
 
-    watchVideo(video) {
+    watch(video) {
       this.VideoService.modal(video);
     }
 
-    deleteVideo(video) {
+    remove(video) {
+      if (this.options.onlyFavourites) {
+        let videoIndexTemp = this.temporaryVideos.indexOf(video);
+        this.StorageService.remove('videos');
+        this.temporaryVideos.splice(videoIndexTemp, 1);
+        let videoIndex = this.videos.indexOf(video);
+        this.videos.splice(videoIndex, 1);
+        return this.StorageService.set('videos', this.temporaryVideos);
+      }
       let videoIndex = this.videos.indexOf(video);
       this.StorageService.remove('videos');
       this.videos.splice(videoIndex, 1);
       this.temporaryVideos.splice(videoIndex, 1);
-      this.StorageService.set('videos', this.videos);
+      this.StorageService.set('videos', this.temporaryVideos);
     }
 
     addToFavourites(video) {
+      if (this.options.onlyFavourites) {
+        let videoIndex = this.temporaryVideos.indexOf(video);
+        video.favourite = !video.favourite;
+        this.StorageService.remove('videos');
+        this.temporaryVideos.splice(videoIndex, 1, video);
+        return this.StorageService.set('videos', this.temporaryVideos);
+      }
       video.favourite = !video.favourite;
       this.StorageService.set('videos', this.videos);
     }
 
-    paginationFilter() {
-      return (this.options.currentPage * this.options.currentPageSize); 
+    paginationFilter(current, size) {
+      return current * size; 
     }
       
   }
