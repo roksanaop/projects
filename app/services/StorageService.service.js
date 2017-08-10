b8af4b12f7e1c6286b8a4d31a824c29dae987847
roksanaop <roksanaop@tlen.pl>
2017-08-10 12:47:33 +0200

    Update StorageService.service.js

--- app/services/StorageService.service.js
+++ app/services/StorageService.service.js
@@ -4,21 +4,24 @@
   class StorageService {
 
     get(keyValue) {
-      return new Promise(resolve => {
+      return new Promise((resolve, reject) => {
         let videoArr = localStorage.getItem(keyValue);
         resolve(JSON.parse(videoArr));
+        reject(new Error('This keyValue does not exist, try with different keyValue'));
       });
     }
 
     set(keyName, keyValue) {
-      return new Promise(resolve => {
+      return new Promise((resolve, reject) => {
         resolve(localStorage.setItem(keyName, JSON.stringify(keyValue)));
+        reject(new Error('Error! Failed setting'));
       });
     }
 
     remove(keyName) {
-      return new Promise(resolve => {
+      return new Promise((resolve, reject) => {
         resolve(localStorage.removeItem(keyName));
+        reject(new Error('Error! Failed removing'));
       });
     }
 
