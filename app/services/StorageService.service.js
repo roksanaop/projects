b4a51247eaf8806f87013273a5e15d836882775f
roksanaop <roksanaop@tlen.pl>
2017-08-24 14:23:26 +0200

    Update StorageService.service.js

--- app/services/StorageService.service.js
+++ app/services/StorageService.service.js
@@ -3,25 +3,37 @@
 
   class StorageService {
 
+    constructor ($timeout) {
+      Object.assign(this, {$timeout});
+    }
+
     get(keyValue) {
       return new Promise((resolve, reject) => {
-        let videoArr = localStorage.getItem(keyValue);
-        resolve(JSON.parse(videoArr));
-        reject(new Error('This keyValue does not exist, try with different keyValue'));
+        this.$timeout(() => {
+          let videoArr = localStorage.getItem(keyValue);
+          resolve(JSON.parse(videoArr));
+          reject(new Error('This keyValue does not exist, try with different keyValue'));
+        }, 1000);
       });
     }
 
     set(keyName, keyValue) {
       return new Promise((resolve, reject) => {
-        resolve(localStorage.setItem(keyName, JSON.stringify(keyValue)));
-        reject(new Error('Error! Failed setting'));
+        this.$timeout((resolve, reject) => {
+          let setArray = localStorage.setItem(keyName, JSON.stringify(keyValue));
+          let videoArr = localStorage.getItem(keyValue);
+          resolve(JSON.parse(videoArr));
+          reject(new Error('Error! Failed setting'));
+        }, 1000);
       });
     }
 
     remove(keyName) {
       return new Promise((resolve, reject) => {
-        resolve(localStorage.removeItem(keyName));
-        reject(new Error('Error! Failed removing'));
+        this.$timeout(() => {
+          resolve(localStorage.removeItem(keyName));
+          reject(new Error('Error! Failed removing'));
+        }, 1000);
       });
     }
 
