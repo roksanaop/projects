37ead49aab85fcd4ba95b6014655bc97c9039756
roksanaop <roksanaop@tlen.pl>
2017-09-18 21:11:23 +0200

    Update StorageService.service.js

--- app/services/StorageService.service.js
+++ app/services/StorageService.service.js
@@ -3,37 +3,55 @@
 
   class StorageService {
 
-    constructor ($interval) {
-      Object.assign(this, {$interval});
+    constructor ($q) {
+      Object.assign(this, {$q});
     }
 
     get(keyName) {
-      return new Promise((resolve, reject) => {
-        this.$interval(() => {
-          let videoArr = localStorage.getItem(keyName);
-          resolve(JSON.parse(videoArr));
+      return this.$q((resolve, reject) => {
+        setTimeout(() => {
+          resolve(JSON.parse(localStorage.getItem(keyName)));
           reject(new Error('This keyName does not exist, try with different keyName'));
         }, 500);
       });
     }
 
-    set(keyName, keyValue) {
-      return new Promise((resolve, reject) => {
-        this.$interval(() => {
-          let setArray = localStorage.setItem(keyName, JSON.stringify(keyValue));
-          let videoArr = localStorage.getItem(keyName);
-          resolve(JSON.parse(videoArr));
-          reject(new Error('Error! Failed setting'));
+    add(keyName, videoData) {
+      return this.$q((resolve, reject) => {
+        setTimeout(() => {
+          let currentArr = JSON.parse(localStorage.getItem(keyName)) || [];
+          currentArr.push(videoData);
+          localStorage.setItem(keyName, JSON.stringify(currentArr));
+          resolve(JSON.parse(localStorage.getItem(keyName)));
+          reject(new Error('Error! Failed adding'));
         }, 500);
       });
     }
 
-    remove(keyName) {
-      return new Promise((resolve, reject) => {
-        this.$interval(() => {
-          let removeArray = localStorage.removeItem(keyName);
-          let videoArr = localStorage.getItem(keyName);
-          resolve(JSON.parse(videoArr));
+    update(keyName, video) {
+      return this.$q((resolve, reject) => {
+        setTimeout(() => {
+          let currentArr = JSON.parse(localStorage.getItem(keyName)) || [];
+          let index = currentArr.findIndex(vid => vid.id === video.id);
+          currentArr.splice(index, 1, video);
+          localStorage.setItem(keyName, JSON.stringify(currentArr));
+          resolve(JSON.parse(localStorage.getItem(keyName)));
+          reject(new Error('Error! Failed updating'));
+        }, 500);
+      });
+    }
+
+    remove(keyName, video) {
+      return this.$q((resolve, reject) => {
+        setTimeout(() => {
+          if (video) {
+            let currentArr = JSON.parse(localStorage.getItem(keyName)) || [];
+            let index = currentArr.findIndex(vid => vid.id === video.id);
+            currentArr.splice(index, 1);
+            return localStorage.setItem(keyName, JSON.stringify(currentArr));
+          }
+          localStorage.removeItem(keyName);
+          resolve(JSON.parse(localStorage.getItem(keyName)));
           reject(new Error('Error! Failed removing'));
         }, 500);
       });
