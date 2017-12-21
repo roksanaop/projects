23a662e9781f9a3589d7443232d7401f620b55dc
roksanaop <roksanaop@tlen.pl>
2017-12-21 14:16:10 +0100

    Update app.module.ts

--- ng2-crypto-trading/src/app/app.module.ts
+++ ng2-crypto-trading/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
-import { OrderedByPipe } from './orderby.pipe';
+import { OrderedByPipe } from './pipes/orderby.pipe';
 import { AppComponent } from './app.component';
 import { CryptoTableComponent } from './crypto-table/crypto-table.component';
-import { CoincapService } from './coincap.service';
-import { SortingService } from './sorting.service';
+import { CoincapService } from './services/coincap.service';
+import { SortingService } from './services/sorting.service';
 
 @NgModule({
   declarations: [
